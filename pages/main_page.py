2ad58010b83f7ce99acadd8860f6cb271278f98e
unknown <karachaev1999@mail.ru>
2025-10-05 13:18:52 +0300

    Page Object in file + new files

---
+++ pages/main_page.py
@@ -0,0 +1,15 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class MainPage(BasePage): 
+    
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
+    
+'''
+Не нужно
+    def go_to_login_page(browser):
+        login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click() 
+'''
--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,25 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
+'''
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
+
+
+def test_guest_can_go_to_login_page(browser, request): 
+   language = request.config.getoption("--language")
+   link = f"http://selenium1py.pythonanywhere.com/{language}/"
+   browser.get(link) 
+   go_to_login_page(browser) 
+'''
 
 def test_guest_can_go_to_login_page(browser, request):
     language = request.config.getoption("--language")
     link = f"http://selenium1py.pythonanywhere.com/{language}/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
