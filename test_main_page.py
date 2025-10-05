04df285a16d2dbe798edc527965ea871379fc659
unknown <karachaev1999@mail.ru>
2025-10-05 13:31:10 +0300

    Добавление нового метода для проверки наличия ссылки

--- test_main_page.py
+++ test_main_page.py
@@ -21,5 +21,6 @@ def test_guest_can_go_to_login_page(browser, request):
     link = f"http://selenium1py.pythonanywhere.com/{language}/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
+    page.should_be_login_link()      # проверяем наличие ссылки
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-
+    
